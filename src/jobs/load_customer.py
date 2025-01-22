83b19636f234504151507cc1c866137d2186e222
Umesh Mahajan <umesh.s.mahajan@gmail.com>
2025-01-22 06:20:15 -0500

    added test coverage with updated git ignore

--- src/jobs/load_customer.py
+++ src/jobs/load_customer.py
@@ -1,3 +1,12 @@
 def addition(a,b):
+    if not isinstance(a, (int, float)) or not isinstance(b,(int, float)):
+        raise TypeError("Input must be either int or float")
     result=a+b
     return result
+
+def substraction(a,b):
+    if not isinstance(a, (int, float)) or not isinstance(b,(int, float)):
+        raise TypeError("Input must be either int or float")
+    result=a-b
+    return result
+
